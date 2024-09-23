968ec6c3e8fb0cc970b49711b7d29024f821a334
LikhitaChilla <163547908+LikhitaChilla@users.noreply.github.com>
2024-09-23 07:19:45 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -7,6 +7,8 @@ from sklearn.preprocessing import PolynomialFeatures
 # Load the pickled model
 with open('poly_regression_modell.pkl', 'rb') as model_file:
     model,feature_names = pickle.load(model_file)
+with open('label_encoder.pkl', 'rb') as model_file:
+    encoder = pickle.load(model_file)
 
 
 # Function to make predictions
@@ -28,13 +30,13 @@ param7 = st.sidebar.number_input('Previous_Scores', value=0)
 param10 = st.sidebar.number_input('Tutoring_Sessions', value=0)
 param15 = st.sidebar.number_input('Physical_Activity',value=0)
 features={
-    'Parental_Involvement':['Low','medium','high'],
-    'Access_to_Resources':['Low','medium','high'],
+    'Parental_Involvement':['Low','Medium','High'],
+    'Access_to_Resources':['Low','Medium','High'],
     'Extracurricular_Activities':['No','Yes'],
-    'Motivation_Level':['Low','medium','high'],
+    'Motivation_Level':['Low','Medium','High'],
     'Internet_Access':['No','Yes'],
-    'Family_Income':['Low','medium','high'],
-    'Teacher_Quality':['Low','medium','high'],
+    'Family_Income':['Low','Medium','High'],
+    'Teacher_Quality':['Low','Medium','High'],
     'School_Type':['Public','Private'],
     'Peer_Influence':['Positive','Negative','Neutral'],
     'Learning_Disabilities':['No','Yes'],
@@ -70,18 +72,36 @@ data = {
     'Distance_from_Home': selected_features['Distance_from_Home'],
     'Gender': selected_features['Gender']
 }
+def load_and_transform(feature, new_value):
+    # Load the corresponding pickled encoder
+    with open(f'{feature}_encoder.pkl', 'rb') as f:
+        encoder = pickle.load(f)
+    
+    # Transform the new user input
+    transformed_value = encoder.transform(new_value)
+    
+    return transformed_value
+
+encoded_user_data = {}
+for feature, value in selected_features.items():
+    data[feature] = load_and_transform(feature,[value])
+
+print("Encoded new user data:", encoded_user_data)
+
 df=pd.DataFrame([data],columns=['Hours_Studied','Attendance','Parental_Involvement','Access_to_Resources','Extracurricular_Activities','Sleep_Hours','Previous_Scores','Motivation_Level','Internet_Access','Tutoring_Sessions','Physical_Activity','Family_Income',
                        'Teacher_Quality','School_Type','Peer_Influence','Learning_Disabilities','Parental_Education_Level','Distance_from_Home','Gender'
                        ])
 
-cat_col=[]
-for i in df.columns:
-    if df[i].dtypes=='object':
-        cat_col.append(i)
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-for col in cat_col:
-    df[col] = le.fit_transform(df[col])
+
+
+# cat_col=[]
+# for i in df.columns:
+#     if df[i].dtypes=='object':
+#         cat_col.append(i)
+# from sklearn.preprocessing import LabelEncoder
+# le = LabelEncoder()
+# for col in cat_col:
+#     df[col] = encoder.transform(df[col])
 df=df[feature_names]
 input_data =df
 # Make prediction on button click
